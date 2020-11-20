034cb374d873b62baa9d4ec9bfc5097626b64f2a
BlinBean <1577429091@qq.com>
2020-11-20 09:29:38 +0800

    add import Texture

--- Star/main.cpp
+++ Star/main.cpp
@@ -4,6 +4,10 @@
 #include <iostream>
 #include "Shader.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+
 //#pragma region shaderCode
 //const char* vertexShaderSource =
 //"#version 330 core													\n"//openGL 版本
@@ -27,14 +31,15 @@
 //#pragma endregion
 
 GLfloat vertices[] = {
-	-0.5f, -0.5f, 0.0f,		1.0,  0,  0,
-	 0.5f, -0.5f, 0.0f,		0,	1.0,  0,
-	 0.0f,  0.5f, 0.0f,		0,	0,	 1.0,
-	 0.8f,	0.8f, 0.0f,		1,	0,	1
+	//     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
 };
 unsigned int indices[] = {
-	0,1,2,
-	2,1,3
+	0,2,1,
+	2,0,3
 };
 
 void processInput(GLFWwindow* window) {
@@ -74,7 +79,7 @@ int main(int argc,char* argv[]) {
 
 
 
-	glViewport(0, 0, 1920, 1080);//窗口可绘制大小，前两个为起始点
+	glViewport(0, 0, 800, 600);//窗口可绘制大小，前两个为起始点
 	glEnable(GL_CULL_FACE);//开启剔除
 	glCullFace(GL_BACK);//背面剔除
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);//多边形模式设为线框
@@ -94,6 +99,45 @@ int main(int argc,char* argv[]) {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);//绑定ELEMENT_ARRAY_BUFFER
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices),indices,GL_STATIC_DRAW);//传入索引值
 #pragma endregion
+
+#pragma region texture
+	unsigned int TexBufferA;
+	glGenTextures(1, &TexBufferA);
+	glActiveTexture(GL_TEXTURE0);//切换贴图通道0，之后的贴图先存在0上
+	glBindTexture(GL_TEXTURE_2D,TexBufferA);//GL_TEXTURE_2D为通道//只要绑定后shader里的sampler2d就可以读取到
+
+	int width, height, nrChannel;
+	//向GPU传入数据//传送数据很浪费带宽，建议一次传完
+	stbi_set_flip_vertically_on_load(true);//y轴反向
+	unsigned char* data = stbi_load("container.jpg",&width,&height,&nrChannel,0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,width,height,0,GL_RGB,GL_UNSIGNED_BYTE,data);//朝通道传输数据//mipmap为第0层//设定保存格式RGB//边距为0//原图格式RGB//data格式(char存的是byte）
+		glGenerateMipmap(GL_TEXTURE_2D);//为通道里的数据创建mipmap
+	}
+	else {
+		std::cout << "load image error" << std::endl;
+	}
+	stbi_image_free(data);
+
+
+	unsigned int TexBufferB;
+	glGenTextures(1, &TexBufferB);
+	glActiveTexture(GL_TEXTURE3);
+	glBindTexture(GL_TEXTURE_2D, TexBufferB);
+	unsigned char* data2 = stbi_load("awesomeface.png",&width,&height,&nrChannel,0);
+
+	if (data2) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data2);
+		glGenerateMipmap(GL_TEXTURE_2D);//为通道里的数据创建mipmap
+	}
+	else {
+		std::cout << "load image error" << std::endl;
+	}
+	stbi_image_free(data2);
+
+
+#pragma endregion
+
 
 //#pragma region Shader
 //	unsigned int vertexShader,fragmentShader,shaderProgram;
@@ -116,14 +160,18 @@ int main(int argc,char* argv[]) {
 //#pragma endregion
 
 	//挖顶点
-	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6*sizeof(GL_FLOAT),(void*)0);//解释顶点（顶点属性）//设置存储在0号，顶点属性数量<1,1,1>,数据类型，是否归一化，数据间的间隔,偏移量
+	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,8*sizeof(GL_FLOAT),(void*)0);//解释顶点（顶点属性）//设置存储在0号，顶点属性数量<1,1,1>,数据类型，是否归一化，数据间的间隔,偏移量
 	//每抓3个为一个顶点
 	glEnableVertexAttribArray(0);//启用顶点属性//0号属性（vertex）//1号color
 	
 	//挖颜色
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (void*)(3*sizeof(float)));//解释顶点（顶点属性）//设置存储在1号，顶点属性数量<1,1,1>,数据类型，是否归一化，数据间的间隔,偏移量
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (void*)(3*sizeof(float)));//解释顶点（顶点属性）//设置存储在1号，顶点属性数量<1,1,1>,数据类型，是否归一化，数据间的间隔,偏移量
 //每抓3个为一个顶点
-	glEnableVertexAttribArray(1);//启用顶点属性//0号属性（vertex）//1号color
+	glEnableVertexAttribArray(1);
+	//挖UV
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (void*)(6*sizeof(float)));
+	glEnableVertexAttribArray(2);
+
 
 
 	while (!glfwWindowShouldClose(window))//是否关闭窗口
@@ -142,8 +190,17 @@ int main(int argc,char* argv[]) {
 		//int vertexColorLocation = glGetUniformLocation(shaderProgram,"ourColor");//从shaderProgram找到位置
 		//glUseProgram(shaderProgram);
 		//glUniform4f(vertexColorLocation,0,greenValue,0,1);//要在确定program后使用//传入变量位置和值
+			//--------
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, TexBufferA);
+		glActiveTexture(GL_TEXTURE3);
+		glBindTexture(GL_TEXTURE_2D, TexBufferB);
 
-		myShader->use();
+		myShader->use();//使用当前Program
+
+		//向shader的uniform传输数据
+		glUniform1i(glGetUniformLocation(myShader->ID,"ourTexture"),0);
+		glUniform1i(glGetUniformLocation(myShader->ID,"ourFace"),3);
 
 		//glDrawArrays(GL_TRIANGLES, 0, 6);//画成三角//从第0个开始画，画6个顶点（三角）
 			//画出EBO
